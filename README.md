7df9975451b5d2bd1d4999b18d12f6750df823a7
Hans Koch <mail@hans-koch.me>
2019-11-25 14:29:16 +0100

    improved example in readme and structured code

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# alias-replacer
+# alias-path-loader
+
+A very simple alias replacement plugin to add alias support to `pug-plain-loader` and similar loaders that don't resolve.
+
+## Hint
+
+This loader does a simple string replace, so be very careful with your alias names
 
 ## Usage
 
@@ -8,12 +14,19 @@
         rules: [
             {
                 test: /\.pug$/,
-                loader: 'alias-replacer',
-                options: {
-                    alias: {
-                        '@img': '/absolute/path/tp/project/src/assets/img',
+                loader: [
+                    {
+                        loader: 'pug-plain-loader'
+                    },
+                    {
+                        loader: 'alias-replacer',
+                        options: {
+                            alias: {
+                                '@img': '/absolute/path/tp/project/src/assets/img',
+                            }
+                        }
                     }
-                }
+                ]
             }
         ]
     }
--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const path = require('path');
 function preparePath(alias, resourcePath, source) {
     for (const aliasName in alias) {
         if (alias.hasOwnProperty(aliasName)) {
-            source = source.replace(new RegExp(aliasName, 'g'), path.relative(path.dirname(resourcePath), alias[aliasName]).replace(/\\/g, '\/'))
+            targetPath = path
+                .relative(path.dirname(resourcePath), alias[aliasName])
+                .replace(/\\/g, '\/')
+            source = source.replace(new RegExp(aliasName, 'g'), targetPath)
         }
     }
 
@@ -15,9 +18,9 @@ module.exports = function(source) {
     const WEBPACK_OPTIONS = loaderUtils.getOptions(this);
 
     // Replace aliases in source
-    const pugString = Object.keys(WEBPACK_OPTIONS.alias).length > 0
-        ? preparePath(WEBPACK_OPTIONS.alias, this.resourcePath, source)
-        : source;
+    if (Object.keys(WEBPACK_OPTIONS.alias).length > 0) {
+        source = preparePath(WEBPACK_OPTIONS.alias, this.resourcePath, source)
+    }
 
-    return pugString;
+    return source;
 }
